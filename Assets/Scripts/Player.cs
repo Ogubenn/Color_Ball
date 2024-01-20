7290dbb112db2a01d089f1e750c94689828bff3e
Oğulcan Durkan <ogulcan285@outlook.com>
2024-01-20 21:22:10 +0300

    Day2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     private Touch touch;
     private Rigidbody rb;
 
-
+    [Range(10, 100)]
     public float speedModifier;
     
 
@@ -30,6 +30,10 @@ public class Player : MonoBehaviour
                 rb.velocity = new Vector3(touch.deltaPosition.x * speedModifier * Time.deltaTime,
                                           transform.position.y,
                                           touch.deltaPosition.y * speedModifier * Time.deltaTime);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                rb.velocity = Vector3.zero;
             }
         }
     }
